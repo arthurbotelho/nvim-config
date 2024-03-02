d184ea739df2bcb2182a29e8cdc0c4169c7a65c9
Arthur Botelho <arthurbotelhocosta@gmail.com>
2024-03-02 11:46:37 -0300

    git config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
       lspconfig.lua_ls.setup {}
       lspconfig.tsserver.setup {}
       lspconfig.gopls.setup {}
+      lspconfig.jsonls.setup {}
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
